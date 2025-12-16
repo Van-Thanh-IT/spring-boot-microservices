6b206daf05661f53f7beea2f740bc4aecb88c027
Van-Thanh-IT <lovanthanh34523@gmail.com>
2025-12-16 23:30:41 +0700

    push

---
+++ order-sevice/src/main/java/com/example/order_sevice/OrderSeviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.order_sevice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderSeviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderSeviceApplication.class, args);
+	}
+
+}
